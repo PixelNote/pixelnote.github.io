af936152f25d8bae816cfa0e29c31807e94d2bbb
Andrés Jiménez <andresito_s2@live.com>
2024-03-12 22:20:30 -0500

    First config

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="w-[100vw] h-[100vh] flex justify-center items-center bg-Tuna">
+      <p className="text-Oxley text-[400px]">Portfolio</p>
+    </main>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      Tuna: "#35374B",
+      Spruce: "#344955",
+      Marble: "#50727B",
+      Oxley: "#78A083",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
